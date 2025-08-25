9e17477e25e08dd74656b7dc72b131618caf93a2
ArjunPraja <prajapatiarjun2801@gmail.com>
2025-08-25 21:31:43 +0530

    Initital commit

--- main.py
+++ main.py
@@ -12,7 +12,19 @@ class RideHailingApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "DeepPurple"
         self.theme_cls.theme_style = "Light"
-        return Builder.load_file("app.kv")
+
+        # Load KV files dynamically
+        Builder.load_file("kv/home.kv")
+        Builder.load_file("kv/login_screen.kv")
+        Builder.load_file("kv/register_screen.kv")
+
+        # Add screens
+        sm = WindowManager()
+        sm.add_widget(HomeScreen(name="home"))
+        sm.add_widget(LoginScreen(name="login"))
+        sm.add_widget(RegisterScreen(name="register"))
+
+        return sm
 
 if __name__ == "__main__":
     RideHailingApp().run()
