0ecc00bbef008965c18bfda55e9c0a83bf9ab6b9
ArjunPraja <prajapatiarjun2801@gmail.com>
2025-08-26 15:18:03 +0530

    Initial Commit

--- screens/users/request_ride.py
+++ screens/users/request_ride.py
@@ -1,7 +1,8 @@
 import random
 from kivymd.uix.screen import MDScreen
 from models.rides_models import insert_ride
-
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 
 class RequestRideScreen(MDScreen):
 
@@ -13,28 +14,38 @@ class RequestRideScreen(MDScreen):
         drop_location = self.ids.drop_input.text.strip()
 
         if not pickup or not drop_location:
-            print("⚠ Please fill in both pickup and drop location")
-            return
+            self.show_popup(f"⚠ Please fill in both pickup and drop location")
+            
+        else:
 
-  
-        fare = random.randint(100, 500)
+    
+            fare = random.randint(100, 500)
 
-       
-        ride_data = {
-            "rider_id": str(user["_id"]),   
-            "driver_id": None,             
-            "pickup": pickup,
-            "drop_location": drop_location,
-            "status": "Requested",
-            "fare": fare
-        }
+        
+            ride_data = {
+                "rider_id": str(user["_id"]),   
+                "driver_id": None,             
+                "pickup": pickup,
+                "drop_location": drop_location,
+                "status": "Requested",
+                "fare": fare
+            }
 
-     
-        result = insert_ride(ride_data)
-        print(f"✅ Ride Requested. ID: {result.inserted_id}")
+        
+            result = insert_ride(ride_data)
+            print(f"✅ Ride Requested. ID: {result.inserted_id}")
 
-      
-        self.ids.pickup_input.text = ""
-        self.ids.drop_input.text = ""
+        
+            self.ids.pickup_input.text = ""
+            self.ids.drop_input.text = ""
 
-        self.manager.current = "user_menu"
+            self.manager.current = "user_menu"
+    
+    def show_popup(self, message):
+        popup = Popup(
+            title='Login Info',
+            content=Label(text=message),
+            size_hint=(None, None),
+            size=(320, 200)
+        )
+        popup.open()
--- screens/users/view_ride_by_id.py
+++ screens/users/view_ride_by_id.py
@@ -1,6 +1,8 @@
 from kivymd.uix.screen import MDScreen
 from models.rides_models import get_ride_by_id
 from bson import ObjectId
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 
 class ViewRideByIdScreen(MDScreen):
 
@@ -8,27 +10,27 @@ class ViewRideByIdScreen(MDScreen):
         ride_id = self.ids.ride_id_input.text.strip()
 
         if not ride_id:
-            self.show_message("⚠ Please enter a Ride ID")
-            return
+            self.show_popup(f"⚠ Please Enter Ride ID Bro")
+            
+        else:
+            try:
+                ride = get_ride_by_id(ObjectId(ride_id))
+            except Exception:
+                self.show_message("❌ Invalid Ride ID")
+                return
 
-        try:
-            ride = get_ride_by_id(ObjectId(ride_id))
-        except Exception:
-            self.show_message("❌ Invalid Ride ID")
-            return
+            if not ride:
+                self.show_message("❌ No Ride Found with this ID")
+                return
 
-        if not ride:
-            self.show_message("❌ No Ride Found with this ID")
-            return
+            # Clear old details
+            self.ids.ride_details_box.clear_widgets()
 
-        # Clear old details
-        self.ids.ride_details_box.clear_widgets()
-
-        # Show ride details in "table-like" format
-        for key, value in ride.items():
-            self.ids.ride_details_box.add_widget(
-                self.make_row(str(key), str(value))
-            )
+            # Show ride details in "table-like" format
+            for key, value in ride.items():
+                self.ids.ride_details_box.add_widget(
+                    self.make_row(str(key), str(value))
+                )
 
     def make_row(self, key, value):
         from kivymd.uix.boxlayout import MDBoxLayout
@@ -42,3 +44,12 @@ class ViewRideByIdScreen(MDScreen):
     def show_message(self, message):
         from kivymd.uix.snackbar import Snackbar
         Snackbar(text=message).open()
+    
+    def show_popup(self, message):
+        popup = Popup(
+            title='Login Info',
+            content=Label(text=message),
+            size_hint=(None, None),
+            size=(320, 200)
+        )
+        popup.open()
