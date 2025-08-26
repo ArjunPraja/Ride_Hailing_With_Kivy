e0d74f7ee32606d19c2b3c6a4e610f32f2283c44
ArjunPraja <prajapatiarjun2801@gmail.com>
2025-08-26 14:27:55 +0530

    Rider  Profile

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from screens.users.request_ride import RequestRideScreen
 from screens.users.view_ride_by_id import ViewRideByIdScreen
 from screens.users.view_my_all_rides import ViewMyAllRidesScreen
 from screens.users.completed_rides import CompletedRidesScreen
+from screens.users.profile import RiderProfileScreen
 # ScreenManager to handle screen transitions
 class WindowManager(ScreenManager):
     user = None 
@@ -30,10 +31,19 @@ class RideHailingApp(MDApp):
         Builder.load_file("kv/register_screen.kv")
         Builder.load_file("kv/user_menu_screen.kv")
         Builder.load_file("kv/driver_menu_screen.kv")
+
+        # Users Operations
         Builder.load_file("kv/users/request_ride_screen.kv")
         Builder.load_file("kv/users/view_ride_by_id_screen.kv")
         Builder.load_file("kv/users/view_my_all_rides_screen.kv")
         Builder.load_file("kv/users/completed_rides_screen.kv")
+        Builder.load_file("kv/users/rider_profile_screen.kv")
+
+        # Driver Operations
+        
+
+
+
         # Create screen manager
         sm = WindowManager()
 
@@ -46,7 +56,7 @@ class RideHailingApp(MDApp):
         sm.add_widget(RequestRideScreen(name="request_ride"))
         sm.add_widget(ViewRideByIdScreen(name="view_ride_by_id"))
         sm.add_widget(ViewMyAllRidesScreen(name="view_my_all_rides"))
-       
+        sm.add_widget(RiderProfileScreen(name="rider_profile"))
         sm.add_widget(CompletedRidesScreen(name="completed_user_rides"))
 
         return sm
